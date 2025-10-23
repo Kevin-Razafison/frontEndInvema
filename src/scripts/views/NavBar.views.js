fd8f715f8c751d8a0f48aa63a4d88ecdda1dd60b
Your Name <angelarknarem@gmail.com>
2025-10-23 07:49:33 +0300

    activate hamburger menu

--- src/scripts/views/NavBar.views.js
+++ src/scripts/views/NavBar.views.js
@@ -51,3 +51,9 @@ export function interactiveNavBar() {
     });
   });
 }
+
+document.querySelector(".hamburger-menu")
+  .addEventListener('click', ()=>{
+      document.querySelector("nav")
+        .style.display = "block";
+  })
